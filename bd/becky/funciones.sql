009ddfd971ba540d5baead9b50108b297f989176
beckysanchez <rebeca.carrillosn@uanl.edu.mx>
2025-11-19 09:21:17 -0600

    si

--- bd/becky/funciones.sql
+++ bd/becky/funciones.sql
@@ -1,24 +1,4 @@
 DELIMITER //
-CREATE FUNCTION fn_edad_usuario(p_usuario_id INT)
-RETURNS INT
-DETERMINISTIC
-READS SQL DATA
-BEGIN
-    DECLARE v_edad INT;
-
-    SELECT TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE())
-    INTO v_edad
-    FROM usuarios
-    WHERE usuario_id = p_usuario_id;
-
-    RETURN v_edad;
-END //
-DELIMITER ;
-
-DROP FUNCTION fn_edad_usuario;
-
-
-DELIMITER //
 CREATE FUNCTION fn_total_reacciones_publicacion(p_publicacion_id INT)
 RETURNS INT
 DETERMINISTIC
